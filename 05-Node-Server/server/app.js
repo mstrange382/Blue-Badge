8f3ffae8ace8b9065f23132f641a8544730e276d
Mitch Strange <mstrange382@gmail.com>
2021-08-12 23:16:48 -0500

    git work

---
+++ 05-Node-Server/server/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+let express = require('express');
+let app = express();
+const sequelize = require("./db");
+let journal = require('./controllers/journalcontroller');
+let user = require('./controllers/usercontroller')
+
+sequelize.sync();
+app.use(require('./middleware/headers'));
+
+
+app.use(express.json());
+
+
+app.use("/user", user);
+
+
+app.use('/journal', journal);
+app.listen(3000, function(){
+    console.log("App is listening on port 3000");
+});
+
+
+
+
+
+
