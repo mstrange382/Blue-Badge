eff059c23a41645532527566f6132f9310284b39
Mitch Strange <mstrange382@gmail.com>
2021-08-18 22:41:14 -0500

    module 7 done

--- react-modules/react-fundamentals/src/App.js
+++ react-modules/react-fundamentals/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Header from './components/site/Header';
 import Footer from './components/site/Footer';
-import FunctionalComponentDemo from './components/concepts/FunctionalComponentDemo';
+//import FunctionalComponentDemo from './components/concepts/FunctionalComponentDemo';
 import Sidebar from './components/site/Sidebar';
 import {
   BrowserRouter as Router,
--- react-modules/react-fundamentals/src/components/site/Sidebar.js
+++ react-modules/react-fundamentals/src/components/site/Sidebar.js
@@ -11,6 +11,7 @@ import JSXRules from "../concepts/JSXRules";
 import State from "../concepts/State";
 import Effects from "../concepts/Effects";
 import PropsDemo from "../concepts/PropsDemo";
+import Hooks from "../concepts/Hooks";
 
 const Sidebar = () => {
     return(
@@ -24,6 +25,7 @@ const Sidebar = () => {
                     <li><Link to ="/state">useState</Link></li>
                     <li><Link to ="/effects">useEffect</Link></li>
                     <li><Link to ="/propsdemo">Props Demo</Link></li>
+                    <li><Link to="/hooks">Hooks</Link></li>
                     
                 </ul>
             </div>
@@ -37,6 +39,7 @@ const Sidebar = () => {
                     <Route exact path="/state"><State /></Route>
                     <Route exact path="/effects"><Effects/></Route>
                     <Route exact path="/propsdemo"><PropsDemo/></Route>
+                    <Route exact path="/hooks"><Hooks></Hooks></Route>
                 </Switch>
             </div>
         </div>
